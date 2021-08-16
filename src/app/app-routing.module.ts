771e2d7debf1496137ca15292a40e5e678b1092c
DESKTOP-N58044P\admin <hungmien0411@gmail.com>
2021-08-16 18:02:06 +0700

    'CRUDProductCategory'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { AdminComponent } from './admin/admin.component';
+import { ProductCategoryComponent } from './admin/product-category/product-category.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path : '' , redirectTo : 'AdminComponent' , pathMatch : 'full'},
+  {path : 'AdminComponent' , component : AdminComponent},
+  {path : 'danhMucSanPham' , component : ProductCategoryComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule , ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { ProductCategoryComponent } from './admin/product-category/product-category.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductCategoryService } from './service/productCategory.service';
+import { AddEditProductCategoryComponent } from './admin/product-category/add-edit-product-category/add-edit-product-category.component';
+
+
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
+import { NgxPaginationModule } from 'ngx-pagination';
+import { CustomERProductCategoryComponent } from './customError/custom-erproduct-category/custom-erproduct-category.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    ProductCategoryComponent,
+    AddEditProductCategoryComponent,
+    CustomERProductCategoryComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+    NgxPaginationModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
-  providers: [],
+  providers: [ProductCategoryService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
